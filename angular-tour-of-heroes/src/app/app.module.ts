76aa85820ab3be0569318efac3ff322e0b177ac0
Jakob Riedl <riedljakob@web.de>
2018-10-31 11:14:18 +0100

    inc travis_5

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MessagesComponent }    from './messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
     // sdf
+    // sdf
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
